d33461cae0fd0f49f69f2bc0234ad213b577a3bc
Daniel Tinney <dnltinney@gmail.com>
2021-04-30 11:03:42 +0200

    add tf module files

---
+++ main.tf
@@ -0,0 +1,77 @@
+terraform {
+  required_version = "~> 0.14.11"
+
+  required_providers {
+    tfe = {
+      source  = "hashicorp/tfe"
+      version = "0.25.0"
+    }
+  }
+
+  experiments = [module_variable_optional_attrs]
+}
+
+# don't set this, for using for_each with module
+# provider "tfe" {
+#   # Configuration options
+# }
+
+resource "tfe_workspace" "workspace" {
+  name                  = var.workspace_name
+  organization          = var.workspace_org
+  auto_apply            = var.workspace_auto_apply
+  file_triggers_enabled = var.workspace_file_triggers_enabled
+  queue_all_runs        = var.workspace_queue_all_runs
+  terraform_version     = var.workspace_terraform_version
+  working_directory     = var.workspace_working_directory
+  trigger_prefixes      = var.workspace_trigger_prefixes
+
+  vcs_repo {
+    branch             = var.workspace_vcs_branch
+    identifier         = var.workspace_vcs_identifier
+    ingress_submodules = var.workspace_vcs_ingress_submodules
+    oauth_token_id     = var.workspace_vcs_oauth_token_id
+  }
+}
+
+resource "tfe_team_access" "acces" {
+  for_each = {
+    for team in var.team_access : team.name => {
+      id     = team.id
+      access = team.access
+    }
+  }
+
+  access       = each.value.access
+  team_id      = each.value.id
+  workspace_id = tfe_workspace.workspace.id
+}
+
+resource "tfe_notification_configuration" "slack" {
+  destination_type = "slack"
+  name             = "tfe workspace notification: ${var.workspace_name}"
+  enabled          = var.slack_enabled
+  url              = var.slack_url
+  triggers         = var.slack_triggers
+  workspace_id     = tfe_workspace.workspace.id
+}
+
+resource "tfe_variable" "variable" {
+  for_each = {
+    for variable in var.tf_variables : variable.name => {
+      category    = variable.category
+      value       = variable.value
+      hcl         = variable.hcl
+      sensitive   = variable.sensitive
+      description = variable.description
+    }
+  }
+
+  category     = each.value.category
+  key          = each.key
+  value        = each.value.value
+  hcl          = each.value.hcl
+  sensitive    = each.value.sensitive
+  description  = each.value.description
+  workspace_id = tfe_workspace.workspace.id
+}
---
+++ variables.tf
@@ -0,0 +1,110 @@
+variable "workspace_name" {
+  type        = string
+  description = "(Required) Name of the workspace."
+}
+
+variable "workspace_org" {
+  type        = string
+  description = "(Required) Name of the organization."
+  default     = "edithcare"
+}
+
+variable "workspace_auto_apply" {
+  type        = bool
+  description = "(Required) Whether destroy plans can be queued on the workspace."
+}
+
+variable "workspace_file_triggers_enabled" {
+  type        = bool
+  description = "(Optional) Whether to filter runs based on the changed files in a VCS push."
+  default     = true
+}
+
+variable "workspace_queue_all_runs" {
+  type        = bool
+  description = "(Optional) Whether the workspace should start automatically performing runs immediately after its creation."
+  default     = false
+}
+
+variable "workspace_terraform_version" {
+  type        = string
+  description = "(Required) The version of Terraform to use for this workspace."
+}
+
+variable "workspace_working_directory" {
+  type        = string
+  description = "(Required) A relative path that Terraform will execute within."
+}
+
+variable "workspace_vcs_branch" {
+  type        = string
+  description = "(Optional) repository branch that Terraform will execute from. Default to `master`"
+  default     = "master"
+}
+
+variable "workspace_trigger_prefixes" {
+  type        = list(string)
+  description = "(Optional) List of repository-root-relative paths which describe all locations to be tracked for changes"
+  default     = []
+}
+
+variable "workspace_vcs_identifier" {
+  type        = string
+  description = "(Required) A reference to your VCS repository in the format <organization>/<repository> where <organization> and <repository> refer to the organization and repository in your VCS provider."
+}
+
+variable "workspace_vcs_ingress_submodules" {
+  type        = bool
+  description = "(Optional) Whether submodules should be fetched when cloning the VCS repository. Defaults to `false`."
+  default     = false
+}
+
+variable "workspace_vcs_oauth_token_id" {
+  type        = string
+  description = "(Required) The VCS Connection (OAuth Connection + Token) to use. This ID can be obtained from a tfe_oauth_client resource."
+}
+
+variable "team_access" {
+  type = list(object({
+    name   = string
+    id     = string
+    access = string
+  }))
+  description = "value"
+  default     = []
+}
+
+variable "slack_enabled" {
+  type        = bool
+  description = "value"
+  default     = false
+}
+
+variable "slack_url" {
+  type        = string
+  description = "value"
+}
+
+variable "slack_triggers" {
+  type        = list(string)
+  description = "value"
+  default = [
+    "run:created",
+    "run:completed",
+    "run:errored",
+    "run:needs_attention",
+  ]
+}
+
+variable "tf_variables" {
+  type = list(object({
+    category    = string
+    name        = string
+    value       = string
+    sensitive   = optional(bool)
+    hcl         = optional(bool)
+    description = optional(string)
+  }))
+  description = "value"
+  default     = []
+}
